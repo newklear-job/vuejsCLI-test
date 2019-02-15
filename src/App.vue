3b3f16b8ee481aa1aefebcdfb60d8770b9cad075
newklear-job <petergerinv@gmail.com>
2019-02-15 14:20:53 +0200

    Multiple Components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-<h1> {{title}} </h1>
-<ninjas></ninjas>
+    <app-header></app-header>
+<app-ninjas></app-ninjas>
+
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
 
 export default {
   components:{
-    'ninjas': Ninjas
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
   },
   data(){
     return {
-      title:'Test App'
+
     }
   }
 }
 </script>
 
 <style>
-h1{
-  color:purple;
-}
+
 </style>
